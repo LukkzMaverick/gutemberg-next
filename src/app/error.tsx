de2acf104e191cf97baa7bbe239b779ca21dd1c4
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2025-01-12 17:44:24 -0300

    modified error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,20 +1,17 @@
 'use client'
 
-import React from 'react';
+import { useRouter } from 'next/navigation'
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
-  React.useEffect(() => {
-    console.error(error); 
-  }, [error]);
-
+export default function Error({}: { error: Error; reset: () => void }) {
+  const router = useRouter();
   return (
       <div className="text-center p-6 bg-white border border-gray-300 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold text-red-600 mb-4">Something went wrong!</h2>
         <button 
-          onClick={() => reset()} 
+          onClick={() => router.push('/')} 
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
-          Try again
+          Go to Home
         </button>
       </div>
   );
