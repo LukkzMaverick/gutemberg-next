be103e1c4e07eb41ab6fff71c9d020b83d4c3ca1
Lucas Oliveira Santana da Cunha <lukkzmaverick@gmail.com>
2025-01-12 17:38:42 -0300

    changed error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -10,7 +10,6 @@ export default function Error({ error, reset }: { error: Error; reset: () => voi
   return (
       <div className="text-center p-6 bg-white border border-gray-300 rounded-lg shadow-lg">
         <h2 className="text-2xl font-semibold text-red-600 mb-4">Something went wrong!</h2>
-        <p className="text-lg text-black mb-6">{error.message}</p>
         <button 
           onClick={() => reset()} 
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
